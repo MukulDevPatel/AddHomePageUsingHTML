7147b4c1cc609c03946d7f8abd751dbee60545e0
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-27 19:00:03 +0530

    [Mukul Dev] Add: We display employee details on employee payroll home page in tabular format from Json object

--- js/home.js
+++ js/home.js
@@ -4,21 +4,102 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 //Template literal ES6 features
 const createInnerHtml = () => {
-    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    const headerHtml = "<th>Profile Pic</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    let empPayrollData = createEmployeePayrollJSON()[5];
     const innerHtml = `${headerHtml}
     <tr>
-        <td><img class="profile" alt="" src="../assets/face2.jpg" style="border-radius: 50%; height: 50px; width: 50px;"></td>
-        <td>Mukul Dev Patel</td>
-        <td>Male</td>
-        <td><div class='dept-label'>Engineer</div>
-            <div class='dept-label'>Sales</div>
+        <td><img class="profile" alt="" src="${empPayrollData._profilePic}" style="border-radius: 50%; height: 50px; width: 50px;"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td><div class='dept-label'>${empPayrollData._department[0]}</div>
+            <div class='dept-label'>${empPayrollData._department[1]}</div>
         </td>
-        <td>30000</td>
-        <td>1 January 2020</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
         <td>
-            <img id="1" onclick="remove(this)" alt="delete" src="../assets/deleteIcon.png">
-            <img id="1" onclick="update(this)" alt="edit" src="../assets/edit.png">
+            <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/deleteIcon.png">
+            <img id="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/edit.png">
         </td>
     </tr>`;
     document.querySelector('#display').innerHTML = innerHtml;
+}
+
+//JSON object to load data in our table
+const createEmployeePayrollJSON =() =>{
+    let empPayrollListLocal = [
+        {
+            _name: 'Mukul dev Patel',
+            _gender: 'Male',
+            _department: [
+                'Engineering',
+                'Sales',
+            ],
+            _salary: '48000',
+            _startDate: '1 August 2020',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/face2.jpg'
+        },
+        {
+            _name: 'Anita',
+            _gender: 'Female',
+            _department: [
+                'Sales'
+            ],
+            _salary: '40000',
+            _startDate: '1 August 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/face1.jpg'
+        },
+        {
+            _name: 'Saurabh raj',
+            _gender: 'Male',
+            _department: [
+                'Engineering'
+            ],
+            _salary: '48000',
+            _startDate: '1 October 2020',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/face5.jpg'
+        },
+        {
+            _name: 'Ajay kumar',
+            _gender: 'Male',
+            _department: [
+                'Finance'
+            ],
+            _salary: '42000',
+            _startDate: '1 June 2020',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/face6.jpg'
+        },
+        {
+            _name: 'Kriti',
+            _gender: 'Female',
+            _department: [
+                'HR'
+            ],
+            _salary: '50000',
+            _startDate: '1 May 2017',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/face3.jpg'
+        },
+        {
+            _name: 'Emma jane',
+            _gender: 'Female',
+            _department: [
+                'Others'
+            ],
+            _salary: '40000',
+            _startDate: '1 February 2018',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/face4.jpg'
+        }
+    ];
+    return empPayrollListLocal;
 }
--- js/utility.js
+++ js/utility.js
@@ -1,6 +1,5 @@
 const stringify = (date) => {
     const options = {day:'numeric', month:'short',year:'numeric'};
-    const newDate = !date ? "undefined":
-     new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
+    const newDate = !date ? "undefined": new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
     return newDate;
 }
